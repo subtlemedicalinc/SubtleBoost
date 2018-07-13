0f54c2e85126f7bb8cc928eaf15800d3ed323b36
Jon Tamir <jon@subtlemedical.com>
2018-07-13 00:25:50 -0700

    add argparse and option to save fig

--- plot_grid.py
+++ plot_grid.py
@@ -4,14 +4,14 @@ import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 
 import train
 import subtle.subtle_plot as suplot
 import subtle.subtle_preprocess as sup
 import subtle.subtle_io as suio
 
-npy_file = sys.argv[1]
-idx = int(sys.argv[2])
+import argparse
 
 def tile(ims):
     return np.stack(ims, axis=2)
@@ -19,29 +19,49 @@ def tile(ims):
 def imshowtile(x, cmap='gray'):
     plt.imshow(x.transpose((0,2,1)).reshape((x.shape[0], -1)),cmap=cmap)
 
-verbose = True
-
-data = suio.load_npy_file(npy_file)
-
-X0 = data[idx,:,:,0]
-X1 = data[idx,:,:,1]
-X2 = data[idx,:,:,2]
-
-plt.figure()
-plt.subplot(3,1,1)
-imshowtile(tile((X0, X1, X2)))
-
-X1mX0 = X1 - X0
-#X1mX0 = np.maximum(X1mX0, 0.)
-
-X2mX0 = X2 - X0
-#X2mX0 = np.maximum(X2mX0, 0.)
+usage_str = 'usage: %(prog)s [options]'
+description_str = 'plot grid of images from dataset'
 
 
-plt.subplot(3,1,2)
-imshowtile(tile((0*X0, X1mX0, X2mX0)))
+if __name__ == '__main__':
 
-plt.subplot(3,1,3)
-imshowtile(tile((X0, X0 + X1mX0, X0 + X2mX0)))
+    parser = argparse.ArgumentParser(usage=usage_str, description=description_str, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--slice', action='store', dest='idx', type=int, help='show this slice (Default -- middle)', default=None)
+    parser.add_argument('--input', action='store', dest='input', type=str, help='input npy file')
+    parser.add_argument('--output', action='store', dest='output', type=str, help='save output instead of plotting', default=None)
 
-plt.show()
+    args = parser.parse_args()
+    
+    data = suio.load_npy_file(args.input)
+
+    if args.idx is None:
+        args.idx = data.shape[0] // 2
+
+    X0 = data[args.idx,:,:,0]
+    X1 = data[args.idx,:,:,1]
+    X2 = data[args.idx,:,:,2]
+
+    plt.figure(figsize=(20,20))
+    plt.subplot(3,1,1)
+    imshowtile(tile((X0, X1, X2)))
+    plt.colorbar()
+
+    X1mX0 = X1 - X0
+    #X1mX0 = np.maximum(X1mX0, 0.)
+
+    X2mX0 = X2 - X0
+    #X2mX0 = np.maximum(X2mX0, 0.)
+
+
+    plt.subplot(3,1,2)
+    imshowtile(tile((0*X0, X1mX0, X2mX0)))
+    plt.colorbar()
+
+    plt.subplot(3,1,3)
+    imshowtile(tile((X0, X0 + X1mX0, X0 + X2mX0)))
+    plt.colorbar()
+
+    if args.output is None:
+        plt.show()
+    else:
+        plt.savefig(args.output)
