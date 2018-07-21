d0febda9619c12e5bfe3e4d6856f16c3eef5890b
Jon Tamir <jon@subtlemedical.com>
2018-07-20 20:10:44 -0700

    batch_preprocess: better options and generate plots

--- scripts/batch_preprocess.sh
+++ scripts/batch_preprocess.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-export dicom_data='/home/subtle/Data/Stanford/lowcon'
-export out_dir='/home/subtle/jon/dev/SubtleGad/data'
+export dicom_data='/data/test_cloud_inference/lowcon'
+export out_dir='/home/ubuntu/jon/dev/data_full/data_small_set'
+export out_dir_plots='/home/ubuntu/jon/dev/data_full/data_small_set/plots'
 export par_jobs=1
+
+mkdir -p ${out_dir}
+mkdir -p ${out_dir_plots}
+
+export max_files=10
 
 function preprocess() {
 
      typeset sub_dir=$1
 
-     echo python preprocess.py --path_base ${dicom_data}/${sub_dir} --verbose --output ${out_dir}/${sub_dir}.npy --discard_start_percent .1 --discard_end_percent .1 > logs_preprocess/log_${sub_dir}.out
-     python preprocess.py --path_base ${dicom_data}/${sub_dir} --verbose --output ${out_dir}/${sub_dir}.npy --discard_start_percent .1 --discard_end_percent .1 > logs_preprocess/log_${sub_dir}.out 2>&1
+     echo python2 /home/ubuntu/jon/dev/SubtleGad/preprocess.py --path_base ${dicom_data}/${sub_dir} --verbose --output ${out_dir}/${sub_dir}.npy --discard_start_percent .1 --discard_end_percent .1 --normalize --normalize_fun mean > logs_preprocess/log_${sub_dir}.out
+     python2 /home/ubuntu/jon/dev/SubtleGad/preprocess.py --path_base ${dicom_data}/${sub_dir} --verbose --output ${out_dir}/${sub_dir}.npy --discard_start_percent .1 --discard_end_percent .1 --normalize --normalize_fun mean > logs_preprocess/log_${sub_dir}.out 2>&1
+
+     python /home/ubuntu/jon/dev/SubtleGad/plot_grid.py --input ${out_dir}/${sub_dir}.npy --output ${out_dir_plots}/${sub_dir}.png
 
 }
 
@@ -17,5 +25,5 @@ export -f preprocess
 
 
 mkdir -p logs_preprocess
-ls $dicom_data | xargs -n1 -P ${par_jobs} -I{} bash -c "preprocess {}"
+ls $dicom_data | grep "Patient" | head -${max_files} | xargs -n1 -P ${par_jobs} -I{} bash -c "preprocess {}"
 
