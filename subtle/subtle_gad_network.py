1961e4da31837ffe853a44df002857b33810c7f1
Jon Tamir <jon@subtlemedical.com>
2018-08-20 12:05:02 -0700

    add rudimentary support for plotting validation image in tensorboard

--- subtle/subtle_gad_network.py
+++ subtle/subtle_gad_network.py
@@ -22,6 +22,7 @@ import numpy as np
 import os
 import time
 
+import subtle.subtle_io as suio
 
 # clean up
 def clear_keras_memory():
@@ -35,6 +36,73 @@ def set_keras_memory(limit=0.9):
     config = tf_ConfigProto()
     config.gpu_options.per_process_gpu_memory_fraction = limit
     set_session(tf_Session(config=config))
+
+def make_image(im):
+    """
+    Convert an numpy representation image to Image protobuf.
+    Copied from https://github.com/lanpa/tensorboard-pytorch/
+    """
+    import imageio
+    import io
+    nx, ny = im.shape
+    im_uint = im.astype(np.uint8)
+    output = io.BytesIO()
+    imageio.imwrite(output, im, format='png')
+    image_string = output.getvalue()
+    output.close()
+    return tf.Summary.Image(height=nx,
+                         width=ny,
+                         encoded_image_string=image_string)
+
+class TensorBoardImageCallback(keras.callbacks.Callback):
+    def __init__(self, model, data_list, slice_dict_list, log_dir, slices_per_epoch=1, slices_per_input=1, batch_size=1, verbose=0, residual_mode=False,
+            max_queue_size=2, num_workers=4, use_multiprocessing=True, shuffle=False, tag='test'):
+        super().__init__() 
+        self.tag = tag
+        self.data_list = data_list
+        self.slice_dict_list = slice_dict_list
+        self.slices_per_epoch = slices_per_epoch
+        self.slices_per_input = slices_per_input
+        self.batch_size = batch_size
+        self.verbose = verbose
+        self.residual_mode = residual_mode
+        self.model = model
+        self.log_dir = log_dir
+        self.max_queue_size  = max_queue_size
+        self.num_workers = num_workers
+        self.use_multiprocessing=use_multiprocessing
+        self.shuffle = shuffle
+
+        self.batch_size = 1
+
+        self._init_generator()
+
+
+    def _init_generator(self):
+        self.generator =  suio.DataGenerator(data_list=self.data_list,
+                batch_size=self.batch_size,
+                shuffle=self.shuffle,
+                verbose=self.verbose, 
+                residual_mode=self.residual_mode,
+                slices_per_input=self.slices_per_input,
+                predict=True)
+
+    def on_epoch_end(self, epoch, logs={}):
+        _len = self.generator.__len__()
+        X = self.generator.__getitem__(_len // 2)
+        #Y_prediction = self.model.predict_generator(generator=self.generator, steps=1, max_queue_size=self.max_queue_size, workers=self.num_workers, use_multiprocessing=self.use_multiprocessing, verbose=self.verbose)
+        Y_prediction = self.model.predict_on_batch(X)
+        if self.residual_mode:
+            h = self.slices_per_input // 2
+            Y_prediction = X[:,:,:,h].squeeze() + Y_prediction.squeeze()
+        image = make_image(Y_prediction.squeeze())
+        summary = tf.Summary(value=[tf.Summary.Value(tag=self.tag, image=image)])
+        writer = tf.summary.FileWriter(self.log_dir)
+        writer.add_summary(summary, epoch)
+        writer.close()
+
+        return
+
 
 class TensorBoardCallBack(keras.callbacks.TensorBoard):
     def __init__(self, log_every=1, **kwargs):
@@ -109,6 +177,23 @@ class DeepEncoderDecoder2D:
             return TensorBoardCallBack(log_every=log_every, log_dir=os.path.join(self.log_dir, '{}_{}'.format(self.job_id, time.time())), batch_size=8, write_graph=False)
         else:
             return keras.callbacks.TensorBoard(log_dir=os.path.join(self.log_dir, '{}_{}'.format(self.job_id, time.time())), batch_size=8, write_graph=False)
+
+    # FIXME check
+    def callback_tbimage(self, data_list, slice_dict_list, slices_per_epoch=1, slices_per_input=1, batch_size=1, verbose=0, residual_mode=False, 
+            max_queue_size=2, num_workers=4, use_multiprocessing=True, tag='test'):
+        return TensorBoardImageCallback(self,
+                data_list=data_list,
+                slice_dict_list=slice_dict_list,
+                log_dir=os.path.join(self.log_dir, '{}_{}'.format(self.job_id, time.time())),
+                slices_per_epoch=slices_per_epoch,
+                slices_per_input=slices_per_input,
+                batch_size=batch_size,
+                verbose=verbose,
+                residual_mode=residual_mode,
+                max_queue_size=max_queue_size,
+                num_workers=num_workers,
+                use_multiprocessing=use_multiprocessing,
+                tag=tag)
 
     def load_weights(self, filename=None):
         if filename is not None:
