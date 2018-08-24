6c7c616132da7b17386bd738fcc38ae886234228
Jon Tamir <jtamir@eecs.berkeley.edu>
2018-08-24 17:18:02 +0000

    make generic file converter

---
+++ scripts/convert_file.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import argparse
+import pathlib
+
+import  subtle.subtle_io as suio
+
+usage_str = 'usage: %(prog)s [options] <input> <output>'
+#description_str = 'pick lines from input and store in output'
+description_str = 'convert file type 1 to file type 2'
+
+parser = argparse.ArgumentParser(usage=usage_str, description=description_str, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('input_file', action='store', type=str, help='input')
+parser.add_argument('output_file', action='store', type=str, help='output')
+parser.add_argument('--key', action='store', dest='key', type=str, help='h5 key', default='data')
+
+args = parser.parse_args()
+
+# infer data type from file extensions
+
+data = suio.load_file(args.input_file, params={'h5_key': args.key})
+
+suffix = ''.join(pathlib.Path(args.output_file).suffixes)
+
+if suffix == '.h5z':
+    suio.save_data_h5(args.output_file, data, h5_key=args.key, compress=True)
+else:
+    suio.save_data(args.output_file, data, params={'h5_key': args.key, 'compress': False})
