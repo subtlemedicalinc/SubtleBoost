5195336a1b8838679434334f29399843708af485
Jon Tamir <jon@subtlemedical.com>
2018-10-09 10:30:06 -0700

    batch_predict: add option to run on single case

--- scripts/batch_predict.sh
+++ scripts/batch_predict.sh
@@ -1,10 +1,16 @@
 export GPU=${GPU:=0}
 export DATA_DIR=${DATA_DIR:="/raid/jon/data_full/data"}
 export DATA_LIST=${DATA_LIST:="/home/subtle/jon/dev/SubtleGad/data_lists/data_val.txt"}
+export DATA_NUMBER=${DATA_NUMBER:="-1"}
 export LOG_DIR=${LOG_DIR:="/raid/jon/logs"}
 export CHECKPOINT_DIR=${CHECKPOINT_DIR:="/raid/jon/checkpoints"}
 export PREDICT_DIR=${PREDICT_DIR:="/raid/jon/predictions"}
 export MAX_DATA_SETS=${MAX_DATA_SETS:=2}
+
+if [[ "${DATA_NUMBER}" != "-1" ]] ; then
+	DATA_LIST="data_list_${DATA_NUMBER}.txt"
+	echo "Patient_${DATA_NUMBER}" > ${DATA_LIST}
+fi
 
 
 export JOB_ID=$1
@@ -26,3 +32,8 @@ for id in `ls -tr ${CHECKPOINT_DIR}/${JOB_ID}_* | xargs -n1 basename | sed -e 's
 	
 	python train.py --data_dir ${DATA_DIR} --data_list ${DATA_LIST} --file_ext npy ${learn_residual_str} ${batch_norm_str} --use_multiprocessing --num_workers 4 --max_queue_size 4 --verbose --max_data_sets ${MAX_DATA_SETS} --batch_size 1 --slices_per_input 5 --gpu ${GPU} --checkpoint ${checkpoint_file} --predict ${predict_file}  --id ${id}
 done
+
+
+if [[ "${DATA_NUMBER}" != "-1" ]] ; then
+	rm -f  ${DATA_LIST}
+fi
