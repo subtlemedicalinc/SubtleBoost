9f9375665977334a6b540f8737a865bb892a4964
Jon Tamir <jon@subtlemedical.com>
2019-02-28 16:50:20 -0800

    add crossval stuff

---
+++ scripts/inference_crossval.sh
@@ -0,0 +1,6 @@
+GPU=$1
+idx=$2
+num_epochs=$3
+
+
+GPU=${GPU} DATA_DIR=/raid/jon/data_full_stanford/data DATA_LIST=data_lists/data_train_stanford_20190203.txt_${idx} DATA_LIST_TEST=data_lists/data_test_stanford_20190203.txt_${idx} DATA_RAW=/home/subtle/Data/Stanford/lowcon PREDICT_DIR=/raid/jon/predictions/dicoms NUM_CHANNEL_FIRST=32 MAX_DATA_SETS=1000 NUM_EPOCHS=${num_epochs} FILE_EXT=h5 VAL_SPLIT=0.05 NUM_WORKERS=4 QUEUE_SIZE=4 LEARN_RESIDUAL=0 SLICES_PER_INPUT=7 BATCH_SIZE=8 LOG_DIR=/raid/jon/logs TB_DIR=/raid/jon/logs_tb HIST_DIR=/raid/jon/history CHECKPOINT_DIR=/raid/jon/checkpoints L1_LAMBDA=.4 SSIM_LAMBDA=.6 NO_SAVE_BEST_ONLY=1 ./scripts/inference.sh
