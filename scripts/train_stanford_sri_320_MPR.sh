fa5f0e2dfa84870e693e768fd76a43aec2e34ae8
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2019-07-29 12:46:03 -0700

    Data lists and scripts for stanford training

---
+++ scripts/train_stanford_sri_320_MPR.sh
@@ -0,0 +1,22 @@
+export GPU=1
+export DATA_DIR=/home/srivathsa/projects/studies/gad/stanford/preprocess/data
+export DATA_LIST=data_lists/data_train_stanford_sri.txt
+export LEARNING_RATE=.001
+export MAX_DATA_SETS=100
+export BATCH_SIZE=8
+export NUM_EPOCHS=100
+export QUEUE_SIZE=4
+export SLICES_PER_INPUT=5
+export SHUFFLE=1
+export FILE_EXT=h5
+export VAL_SPLIT=0.2
+export LOG_DIR=/home/srivathsa/projects/studies/gad/stanford/train/logs
+export TB_DIR=/home/srivathsa/projects/studies/gad/stanford/train/tb
+export HIST_DIR=/home/srivathsa/projects/studies/gad/stanford/train/history
+export CHECKPOINT_DIR=/home/srivathsa/projects/studies/gad/stanford/train/checkpoints
+export L1_LAMBDA=.8
+export SSIM_LAMBDA=.2
+export NO_SAVE_BEST_ONLY=1
+export TRAIN_MPR=1
+export RESAMPLE_SIZE=320
+./scripts/train.sh
