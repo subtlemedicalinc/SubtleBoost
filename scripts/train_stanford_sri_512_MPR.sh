a64ea9d03264ecda23ef7617091540cd9bf86177
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2019-08-23 09:53:06 -0700

    Stanford: Remove cases with no enhancement in 10%

--- scripts/train_stanford_sri_512_MPR.sh
+++ scripts/train_stanford_sri_512_MPR.sh
@@ -1,4 +1,4 @@
-export GPU=1
+export GPU=0
 export DATA_DIR=/home/srivathsa/projects/studies/gad/stanford/preprocess/data
 export DATA_LIST=data_lists/data_train_stanford_sri.txt
 export LEARNING_RATE=.001
@@ -9,7 +9,7 @@ export QUEUE_SIZE=4
 export SLICES_PER_INPUT=5
 export SHUFFLE=1
 export FILE_EXT=h5
-export VAL_SPLIT=0.2
+export VAL_SPLIT=0.25
 export LOG_DIR=/home/srivathsa/projects/studies/gad/stanford/train/logs
 export TB_DIR=/home/srivathsa/projects/studies/gad/stanford/train/tb
 export HIST_DIR=/home/srivathsa/projects/studies/gad/stanford/train/history
