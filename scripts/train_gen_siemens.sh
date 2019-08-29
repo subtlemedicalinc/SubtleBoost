c1eaad001dc89db3f34d5337c0b1bc03ec7adc8c
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2019-08-29 15:30:12 -0700

    Scripts and data files for generalized siemens model (Stanford + Hoag)

---
+++ scripts/train_gen_siemens.sh
@@ -0,0 +1,22 @@
+export GPU=2
+export DATA_DIR=/home/srivathsa/projects/studies/gad/gen_siemens/preprocess/data
+export DATA_LIST=data_lists/data_train_gen_siemens.txt
+export LEARNING_RATE=.001
+export MAX_DATA_SETS=100
+export BATCH_SIZE=8
+export NUM_EPOCHS=100
+export QUEUE_SIZE=4
+export SLICES_PER_INPUT=5
+export SHUFFLE=1
+export FILE_EXT=h5
+export VAL_SPLIT=0.2
+export LOG_DIR=/home/srivathsa/projects/studies/gad/gen_siemens/train/logs
+export TB_DIR=/home/srivathsa/projects/studies/gad/gen_siemens/train/tb
+export HIST_DIR=/home/srivathsa/projects/studies/gad/gen_siemens/train/history
+export CHECKPOINT_DIR=/home/srivathsa/projects/studies/gad/gen_siemens/train/checkpoints
+export L1_LAMBDA=.8
+export SSIM_LAMBDA=.2
+export NO_SAVE_BEST_ONLY=1
+export TRAIN_MPR=1
+export RESIZE=256
+./scripts/train.sh
