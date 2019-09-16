b13af9597764c6911fd8a5b0b1b50eba9b170cbd
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2019-09-16 13:35:20 -0700

    Log file name as git commit and config file SHA

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,4 @@
-timestamp=$(date "+%s")
-logfile=$2/log_$1_${timestamp}.log
+commit=${commit:=$(git rev-parse HEAD | cut -c1-6)}
+job_id=$(cat experiments/$1/config.json | python3 -c "import sys, json; print(json.load(sys.stdin)['inference'])" | sha1sum | awk '{print $1}' | cut -c1-6)
+logfile=$2/log_train_${commit}_${job_id}.log
 python train_process.py --experiment $1 > ${logfile} 2>${logfile}
