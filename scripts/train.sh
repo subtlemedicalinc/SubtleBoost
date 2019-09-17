985b44e84fc1465ecb8f46279a1d966870a9c6b7
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2019-09-17 10:08:20 -0700

    Handle case when sub experiment is not present in command line

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,6 +4,9 @@ exparg=$1
 if [[ $exparg == *"/"* ]]; then
   expname="$(cut -d'/' -f1 <<<"$exparg")"
   subexp="$(cut -d'/' -f2 <<<"$exparg")"
+  exp_str="--experiment ${expname} --sub_experiment ${subexp}"
+else
+  exp_str="--experiment ${exparg}"
 fi
 
 fcontent=$(cat experiments/${expname}/config.json | python3 -c "import sys, json; print(json.dumps(json.load(sys.stdin)['inference'], sort_keys=True))")
@@ -12,4 +15,4 @@ fcontent=${fcontent}--${exparg}
 job_id=$(echo ${fcontent} | sha1sum | awk '{print $1}' | cut -c1-6)
 logfile=$2/log_train_${commit}_${job_id}.log
 
-python train_process.py --experiment ${expname} --sub_experiment ${subexp} > ${logfile} 2>${logfile}
+python train_process.py ${exp_str} > ${logfile} 2>${logfile}
