ddacf0ad17e0ae02dc63e74401acc62ce9bbd501
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2019-09-17 14:07:40 -0700

    Re organize py files in scripts folder

--- scripts/preprocess_example.sh
+++
@@ -1,1 +0,0 @@
-python preprocess.py --path_zero /home/subtle/Data/Stanford/lowcon/Patient_0121/7_AX_BRAVO_PRE --path_low /home/subtle/Data/Stanford/lowcon/Patient_0121/10_AX_BRAVO_+C --path_full /home/subtle/Data/Stanford/lowcon/Patient_0121/13_AX_BRAVO+C --verbose --output data/Patient_0121.npy --discard_start_percent .1 --discard_end_percent .1
