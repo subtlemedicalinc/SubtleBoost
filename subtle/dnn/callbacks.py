d2b2ec0c0f8e3b42672c9bf60e3b7e8694c84171
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2019-09-24 10:39:21 -0700

    Fix tensorboard image callback to support 3D blocks

--- subtle/dnn/callbacks.py
+++ subtle/dnn/callbacks.py
@@ -4,10 +4,11 @@ import tensorflow as tf
 import keras
 
 from subtle.data_loaders import SliceLoader
-from subtle.dnn.helpers import make_image
+from subtle.dnn.helpers import make_image, load_data_loader
+from scipy.misc import imresize
 
 class TensorBoardImageCallback(keras.callbacks.Callback):
-    def __init__(self, model, data_list, slice_dict_list, log_dir, slices_per_epoch=1, slices_per_input=1, batch_size=1, verbose=0, residual_mode=False, max_queue_size=2, num_workers=4, use_multiprocessing=True, shuffle=False, tag='test', gen_type='legacy', positive_only=False, image_index=None, mode='random', input_idx=[0,1], output_idx=[2], resize=None, slice_axis=[0], resample_size=None, brain_only=None, brain_only_mode=None):
+    def __init__(self, model, data_list, slice_dict_list, log_dir, slices_per_epoch=1, slices_per_input=1, batch_size=1, verbose=0, residual_mode=False, max_queue_size=2, num_workers=4, use_multiprocessing=True, shuffle=False, tag='test', gen_type='legacy', positive_only=False, image_index=None, mode='random', input_idx=[0,1], output_idx=[2], resize=None, slice_axis=[0], resample_size=None, brain_only=None, brain_only_mode=None, model_name=None, block_size=64, block_strides=32):
         super().__init__()
         self.tag = tag
         self.data_list = data_list
@@ -34,28 +35,46 @@ class TensorBoardImageCallback(keras.callbacks.Callback):
         self.resample_size = resample_size
         self.brain_only = brain_only
         self.brain_only_mode = brain_only_mode
+        self.model_name = model_name
+        self.block_size = block_size
+        self.block_strides = block_strides
 
         self._init_generator()
 
 
     def _init_generator(self):
-            self.generator =  SliceLoader(data_list=self.data_list,
-                    batch_size=1,
-                    shuffle=self.shuffle,
-                    verbose=self.verbose,
-                    residual_mode=self.residual_mode,
-                    positive_only = self.positive_only,
-                    slices_per_input=self.slices_per_input,
-                    input_idx=self.input_idx,
-                    output_idx=self.output_idx,
-                    predict=False,
-                    resize=self.resize,
-                    resample_size=self.resample_size,
-                    slice_axis=self.slice_axis,
-                    brain_only=self.brain_only,
-                    brain_only_mode=self.brain_only_mode)
+        data_loader = load_data_loader(self.model_name)
+        gen_kwargs = {
+            'data_list': self.data_list,
+            'batch_size': 1,
+            'shuffle': self.shuffle,
+            'verbose': self.verbose,
+            'predict': False,
+            'brain_only': self.brain_only,
+            'brain_only_mode': self.brain_only_mode
+        }
 
-            self.img_indices = np.random.choice(range(self.generator.__len__()), size=self.batch_size, replace=False)
+        if '3d' in self.model_name:
+            kw = {
+                'block_size': self.block_size,
+                'block_strides': self.block_strides
+            }
+        else:
+            kw = {
+                'residual_mode': self.residual_mode,
+                'positive_only':  self.positive_only,
+                'slices_per_input': self.slices_per_input,
+                'input_idx': self.input_idx,
+                'output_idx': self.output_idx,
+                'slice_axis': self.slice_axis,
+                'resize': self.resize,
+                'resample_size': self.resample_size,
+            }
+
+        gen_kwargs = {**gen_kwargs, **kw}
+        self.generator =  data_loader(**gen_kwargs)
+
+        self.img_indices = np.random.choice(range(self.generator.__len__()), size=self.batch_size, replace=False)
 
     def on_epoch_end(self, epoch, logs={}):
         #_len = self.generator.__len__()
@@ -68,26 +87,34 @@ class TensorBoardImageCallback(keras.callbacks.Callback):
             raw_data=False
             # enforce_raw_data=(ii >= 5) [uncomment when using masked + full head mixed model]
 
-            X, Y = self.generator.__getitem__(ii, enforce_raw_data=raw_data)
-            Y_prediction = self.model.predict_on_batch(X)
+            if '3d' in self.model_name:
+                X, Y, _ = self.generator.__getitem__(ii, enforce_raw_data=raw_data)
+                Y_prediction = self.model.predict_on_batch(X)
 
-            if self.generator._current_file_list:
-                current_fpath = self.generator._current_file_list[0]
-                case_num = current_fpath.split('/')[-1].replace('.h5', '')
-                slice_idx = self.generator._current_slice_list[0]
-                tag = '{} ({}: slice={}_{})'.format(tag, case_num, slice_idx['axis'], slice_idx['index'])
+                pidx = X.shape[1] // 2
+                X_zero = X[0, pidx, :, :, 0]
+                X_low = X[0, pidx, :, :, 1]
+                Y_full = Y[0, pidx, :, :, 0]
+                Y_pred_full = Y_prediction[0, pidx, :, :, 0]
 
-            #print(X.shape, Y.shape, Y_prediction.shape)
-            # separate 2.5D and N
-            X = np.reshape(X, (X.shape[0], X.shape[1], X.shape[2], self.slices_per_input, len(self.input_idx)))
-            h = self.slices_per_input // 2
+                display_image = np.hstack([X_zero, X_low, Y_full, Y_pred_full])
+                display_image = imresize(display_image, (display_image.shape[0]*3, display_image.shape[1]*3))
+            else:
+                X, Y = self.generator.__getitem__(ii, enforce_raw_data=raw_data)
+                Y_prediction = self.model.predict_on_batch(X)
 
-            X_center = X[...,h,:] # [1, nx, ny, N]
-            if self.gen_type == 'legacy' and self.residual_mode and len(self.input_idx) == 2:
-                Y_prediction = X_center[..., 0] + Y_prediction
-                Y = X_center[..., 0] + Y
-                X_center[..., 1] = X_center[..., 1] + X_center[..., 0]
-            display_image = np.concatenate((X_center, Y, Y_prediction), axis=3).transpose((0,1,3,2)).reshape((X_center.shape[1], -1))
+                X = np.reshape(X, (X.shape[0], X.shape[1], X.shape[2], self.slices_per_input, len(self.input_idx)))
+
+                h = self.slices_per_input // 2
+                X_center = X[...,h,:] # [1, nx, ny, N]
+
+                if self.gen_type == 'legacy' and self.residual_mode and len(self.input_idx) == 2:
+                    Y_prediction = X_center[..., 0] + Y_prediction
+                    Y = X_center[..., 0] + Y
+                    X_center[..., 1] = X_center[..., 1] + X_center[..., 0]
+
+                display_image = np.concatenate((X_center, Y, Y_prediction), axis=3).transpose((0,1,3,2)).reshape((X_center.shape[1], -1))
+
             image = make_image(display_image)
             summary = tf.Summary(value=[tf.Summary.Value(tag=tag, image=image)])
             writer.add_summary(summary, epoch)
--- subtle/dnn/generators/base.py
+++ subtle/dnn/generators/base.py
@@ -67,7 +67,7 @@ class GeneratorBase:
         else:
             return keras.callbacks.TensorBoard(log_dir=_log_dir, batch_size=8, write_graph=False)
 
-    def callback_tbimage(self, data_list, slice_dict_list, slices_per_epoch=1, slices_per_input=1, batch_size=1, verbose=0, residual_mode=False, max_queue_size=2, num_workers=4, use_multiprocessing=True, tag='test', gen_type='legacy', log_dir=None, shuffle=False, image_index=None, input_idx=[0,1], output_idx=[2], slice_axis=0, resize=None, resample_size=None, brain_only=None, brain_only_mode=None):
+    def callback_tbimage(self, data_list, slice_dict_list, slices_per_epoch=1, slices_per_input=1, batch_size=1, verbose=0, residual_mode=False, max_queue_size=2, num_workers=4, use_multiprocessing=True, tag='test', gen_type='legacy', log_dir=None, shuffle=False, image_index=None, input_idx=[0,1], output_idx=[2], slice_axis=0, resize=None, resample_size=None, brain_only=None, brain_only_mode=None, model_name=None, block_size=64, block_strides=16):
         if log_dir is None:
             _log_dir = self.log_dir
         else:
@@ -94,7 +94,10 @@ class GeneratorBase:
                 resize=resize,
                 resample_size=resample_size,
                 brain_only=brain_only,
-                brain_only_mode=brain_only_mode)
+                brain_only_mode=brain_only_mode,
+                model_name=model_name,
+                block_size=block_size,
+                block_strides=block_strides)
 
     def load_weights(self, filename=None):
         if filename is not None:
