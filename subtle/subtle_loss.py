870675fe6b19647cc4014f327719200c793a6ef0
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2019-10-07 10:49:20 -0700

    SSIM loss implementation for 3D patches

--- subtle/subtle_loss.py
+++ subtle/subtle_loss.py
@@ -14,20 +14,61 @@ import warnings
 import time
 import numpy as np
 
-#from skimage.measure import compare_ssim, compare_psnr
-
-try:
-    from tensorflow import log as tf_log
-    from tensorflow import constant as tf_constant
-    import keras.losses
-    from keras import backend as K
-except:
-    warnings.warn('import keras failed')
-
-from keras_contrib.backend import extract_image_patches as subtle_extract_image_patches
+import tensorflow as tf
+from tensorflow import log as tf_log
+from tensorflow import constant as tf_constant
+import keras.losses
+from keras import backend as K
 
 from keras.applications.vgg19 import VGG19, preprocess_input as vgg_preprocess
 from keras.models import Model
+
+"""
+Based on implementation from https://github.com/keras-team/keras-contrib/blob/master/keras_contrib/backend/tensorflow_backend.py
+
+Modified by Srivathsa Pasumarthi for 3D patches
+"""
+def extract_image_patches(x, ksizes, ssizes, padding='same',
+                          data_format='channels_last'):
+    """Extract the patches from an image.
+    # Arguments
+        x: The input image
+        ksizes: 2-d tuple with the kernel size
+        ssizes: 2-d tuple with the strides size
+        padding: 'same' or 'valid'
+        data_format: 'channels_last' or 'channels_first'
+    # Returns
+        The (k_w,k_h) patches extracted
+        TF ==> (batch_size,w,h,k_w,k_h,c)
+        TH ==> (batch_size,w,h,c,k_w,k_h)
+    """
+
+    if K.ndim(x) == 5:
+        bs_i, d_i, w_i, h_i, ch_i = K.int_shape(x)
+        kernel = [1] + [ksizes[0]] * 3 + [1]
+        strides = [1] + [ssizes[0]] * 3 + [1]
+
+        patches = tf.extract_volume_patches(x, kernel, strides, padding)
+        bs, d, w, h, ch = K.int_shape(patches)
+        reshaped = tf.reshape(patches, [-1, d, w, h, tf.floordiv(ch, ch_i), ch_i])
+        final_shape = [-1, d, w, h, ch_i, ksizes[0], ksizes[0], ksizes[0]]
+        patches = tf.reshape(tf.transpose(reshaped, [0, 1, 2, 3, 5, 4]), final_shape)
+
+        patches = K.permute_dimensions(patches, [0, 1, 2, 3, 5, 6, 7, 4])
+    elif K.ndim(x) == 4:
+        bs_i, w_i, h_i, ch_i = K.int_shape(x)
+        kernel = [1, ksizes[0], ksizes[1], 1]
+        strides = [1, ssizes[0], ssizes[1], 1]
+
+        patches = tf.extract_image_patches(x, kernel, strides, [1, 1, 1, 1], padding)
+        bs, w, h, ch = K.int_shape(patches)
+        reshaped = tf.reshape(patches, [-1, w, h, tf.floordiv(ch, ch_i), ch_i])
+        final_shape = [-1, w, h, ch_i, ksizes[0], ksizes[1]]
+        patches = tf.reshape(tf.transpose(reshaped, [0, 1, 2, 4, 3]), final_shape)
+
+        patches = K.permute_dimensions(patches, [0, 1, 2, 4, 5, 3])
+
+    return patches
 
 def ssim_loss(y_true, y_pred, kernel=(3, 3), k1=.01, k2=.03, kernel_size=3, max_value=1.):
     # ssim parameters
@@ -38,13 +79,17 @@ def ssim_loss(y_true, y_pred, kernel=(3, 3), k1=.01, k2=.03, kernel_size=3, max_
     y_true = K.reshape(y_true, [-1] + list(K.int_shape(y_pred)[1:]))
     y_pred = K.reshape(y_pred, [-1] + list(K.int_shape(y_pred)[1:]))
 
-    patches_true = subtle_extract_image_patches(y_true, kernel, kernel, 'valid', K.image_data_format())
-    patches_pred = subtle_extract_image_patches(y_pred, kernel, kernel, 'valid', K.image_data_format())
+    patches_true = extract_image_patches(y_true, kernel, kernel, 'VALID', K.image_data_format())
+    patches_pred = extract_image_patches(y_pred, kernel, kernel, 'VALID', K.image_data_format())
 
-    bs, w, h, c1, c2, c3 = K.int_shape(patches_pred)
-
-    patches_true = K.reshape(patches_true, [-1, w, h, c1 * c2 * c3])
-    patches_pred = K.reshape(patches_pred, [-1, w, h, c1 * c2 * c3])
+    if K.ndim(y_true) == 4:
+        bs, w, h, c1, c2, c3 = K.int_shape(patches_pred)
+        patches_true = K.reshape(patches_true, [-1, w, h, c1 * c2 * c3])
+        patches_pred = K.reshape(patches_pred, [-1, w, h, c1 * c2 * c3])
+    elif K.ndim(y_true) == 5:
+        bs, d, w, h, c1, c2, c3, c4 = K.int_shape(patches_pred)
+        patches_true = K.reshape(patches_true, [-1, d, w, h, c1 * c2 * c3 * c4])
+        patches_pred = K.reshape(patches_pred, [-1, d, w, h, c1 * c2 * c3 * c4])
 
     # Get mean
     u_true = K.mean(patches_true, axis=-1)
