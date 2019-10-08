4eca8f6d0ada0b90313c0e4d500c3c7049e9d0c8
Jon Tamir <jon@subtlemedical.com>
2019-10-08 13:21:42 -0700

    usage/help for helper scripts

--- scripts/batch_preprocess.sh
+++ scripts/batch_preprocess.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
+function show_usage() {
+	echo "Usage: $0 experiment_name log_dir"
+}
+
+function show_help() {
+	echo ""
+	echo "SubtleGad batch preprocessing convenience script."
+	echo ""
+}
+
+OPTIND=1
+
+if [[ $# -ne 2 ]] ; then
+	show_usage >&2
+	exit 1
+fi
+
+while getopts "h?" opt; do
+	case "${opt}" in
+		h)
+			show_usage >&1
+			show_help >&1
+			exit 0
+			;;
+		\?)
+			show_usage >&2
+			exit 1
+			;;
+	esac
+done
+
+shift "$((OPTIND-1))"
+
 logfile=$2/preprocess.out
 errfile=$2/preprocess.err
 
--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
+function show_usage() {
+	echo "Usage: $0 experiment_name log_dir"
+}
+
+function show_help() {
+	echo ""
+	echo "SubtleGad inference convenience script."
+	echo ""
+}
+
+OPTIND=1
+
+if [[ $# -ne 2 ]] ; then
+	show_usage >&2
+	exit 1
+fi
+
+while getopts "h?" opt; do
+	case "${opt}" in
+		h)
+			show_usage >&1
+			show_help >&1
+			exit 0
+			;;
+		\?)
+			show_usage >&2
+			exit 1
+			;;
+	esac
+done
+
+shift "$((OPTIND-1))"
+
 commit=${commit:=$(git rev-parse HEAD | cut -c1-6)}
 exparg=$1
 
