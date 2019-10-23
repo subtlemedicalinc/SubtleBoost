d922d40aa9c81383e26e64b867ef2ea1ffed0283
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2019-10-23 08:24:59 -0700

    Hyperparam test script - WIP - switch to test_tube

--- hyperparam.py
+++ hyperparam.py
@@ -3,34 +3,36 @@ from subtle.dnn.generators import GeneratorUNet2D
 from subtle.data_loaders import SliceLoader
 import subtle.subtle_loss as suloss
 
-import ray
-from ray import tune
-from ray.tune.schedulers import AsyncHyperBandScheduler
-import keras
-from ray.tune import track
-
-ray.init()
+from test_tube import Experiment, HyperOptArgumentParser
 
 fpaths_h5 = [
     '/home/srivathsa/projects/studies/gad/tiantan/preprocess/data/NO26.h5',
     '/home/srivathsa/projects/studies/gad/tiantan/preprocess/data/NO27.h5'
 ]
 
-def train(params, reporter):
-    l1_w = params['l1_lambda']
+def train(params, gpu_ids):
+    print('GPU IDS...', gpu_ids)
+    exp = Experiment(
+        name=params.test_tube_exp_name,
+        save_dir='/home/srivathsa/projects/studies/gad/tiantan/train/logs',
+        autosave=False
+    )
+    exp.argparse(params)
+
+
+    l1_w = params.l1_lambda
     ssim_w = 1 - l1_w
     loss_function = suloss.mixed_loss(l1_lambda=l1_w, ssim_lambda=ssim_w)
     metrics_monitor = [suloss.l1_loss, suloss.ssim_loss, suloss.mse_loss]
 
     data_loader = SliceLoader(
-        data_list=fpaths_h5, batch_size=params['batch_size'], shuffle=False, verbose=0,
-        slices_per_input=7, resize=240, slice_axis=[0]
+        data_list=fpaths_h5, batch_size=params.batch_size, shuffle=False, verbose=0, slices_per_input=7, resize=240, slice_axis=[0]
     )
 
     model = GeneratorUNet2D(
         num_channel_output=1,
         loss_function=loss_function, metrics_monitor=metrics_monitor,
-        verbose=0, lr_init=params['lr_init'],
+        verbose=0, lr_init=params.lr_init,
         img_rows=240, img_cols=240, num_channel_input=14, compile_model=True
     )
     model.load_weights()
@@ -38,63 +40,17 @@ def train(params, reporter):
     train_X, train_Y = data_loader.__getitem__(7)
     val_X, val_Y = data_loader.__getitem__(14)
 
-    return model.model.fit(train_X, train_Y, validation_data=(val_X, val_Y), verbose=0, callbacks=[TuneReporterCallback(reporter)])
+    out = model.model.fit(train_X, train_Y, validation_data=(val_X, val_Y), verbose=0)
 
-class TuneReporterCallback(keras.callbacks.Callback):
-    """Tune Callback for Keras."""
-
-    def __init__(self, reporter=None, freq="batch", metrics=None, logs={}):
-        """Initializer.
-        Args:
-            reporter (StatusReporter|tune.track.log|None): Tune object for
-                returning results.
-            freq (str): Sets the frequency of reporting intermediate results.
-                One of ["batch", "epoch"].
-        """
-        self.reporter = reporter or track.log
-        self.iteration = 0
-        self.metrics = metrics or None
-        if freq not in ["batch", "epoch"]:
-            raise ValueError("{} not supported as a frequency.".format(freq))
-        self.freq = freq
-        super(TuneReporterCallback, self).__init__()
-
-    def on_batch_end(self, batch, logs={}):
-        if not self.freq == "batch":
-            return
-        self.iteration += 1
-        # for metric in list(logs):
-        #     if "loss" in metric and "neg_" not in metric:
-        #         logs["neg_" + metric] = -logs[metric]
-        print('logs....', logs)
-        self.reporter(keras_info=logs, mean_accuracy=-logs.get('l1_loss'))
-
-    def on_epoch_end(self, batch, logs={}):
-        if not self.freq == "epoch":
-            return
-        self.iteration += 1
-        # for metric in list(logs):
-        #     if "loss" in metric and "neg_" not in metric:
-        #         logs["neg_" + metric] = -logs[metric]
-        self.reporter(keras_info=logs, mean_accuracy=-logs.get("val_l1_loss"))
+    exp.log({'result': np.array(out.history['val_l1_loss'])})
+    exp.save()
 
 if __name__ == '__main__':
-    sched = AsyncHyperBandScheduler(
-        metric="val_l1_loss",
-        mode="min"
-    )
+    parser = HyperOptArgumentParser(strategy='random_search')
+    parser.add_argument('--test_tube_exp_name', default='hyp_test')
+    parser.opt_range('--l1_lambda', default=0.6, type=float, tunable=True, low=0.0, high=1.0, nb_samples=20)
+    parser.opt_list('--batch_size', default=8, type=int, tunable=True, options=[4, 8, 12, 16])
+    parser.opt_range('--lr_init', default=0.001, type=float, tunable=True, low=0.1, high=0.001, nb_samples=20)
 
-    tune.run(
-        train,
-        name="exp",
-        scheduler=sched,
-        num_samples=2,
-        config={
-            "num_workers": 2,
-            "num_gpus": 2,
-            "num_gpus_per_worker": 1,
-            "num_cpus": 0,
-            "l1_lambda": tune.sample_from(lambda spec: np.random.uniform(0, 1)),
-            "batch_size": tune.sample_from(lambda spec: [4, 8, 12][np.random.randint(3)]),
-            "lr_init": tune.sample_from(lambda spec: np.random.uniform(0.001, 0.1))
-        })
+    hyp_params = parser.parse_args()
+    hyp_params.optimize_parallel_gpu(train, gpu_ids=['0', '1'], max_nb_trials=10)
