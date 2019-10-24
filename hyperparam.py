fe7c5dc584959fde91244a17b79a61ee0b3d620b
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2019-10-24 13:25:07 -0700

    Hyperparam test - more on tb callback WIP

--- hyperparam.py
+++ hyperparam.py
@@ -11,7 +11,6 @@ fpaths_h5 = [
 ]
 
 def train(params, gpu_ids):
-    print('GPU IDS...', gpu_ids)
     exp = Experiment(
         name=params.test_tube_exp_name,
         save_dir='/home/srivathsa/projects/studies/gad/tiantan/train/logs',
@@ -40,17 +39,21 @@ def train(params, gpu_ids):
     train_X, train_Y = data_loader.__getitem__(7)
     val_X, val_Y = data_loader.__getitem__(14)
 
-    out = model.model.fit(train_X, train_Y, validation_data=(val_X, val_Y), verbose=0)
-
-    exp.log({'result': np.array(out.history['val_l1_loss'])})
+    for i in range(5):
+        out = model.model.fit(train_X, train_Y, validation_data=(val_X, val_Y), verbose=0)
+        print('history', out.history)
+        val_l1_loss = np.array(out.history['val_l1_loss'])
+        print('VAL LOSS', val_l1_loss)
+        exp.log({'val_l1_loss': val_l1_loss})
     exp.save()
+    exp.close()
 
 if __name__ == '__main__':
     parser = HyperOptArgumentParser(strategy='random_search')
-    parser.add_argument('--test_tube_exp_name', default='hyp_test')
+    parser.add_argument('--test_tube_exp_name', default='test')
     parser.opt_range('--l1_lambda', default=0.6, type=float, tunable=True, low=0.0, high=1.0, nb_samples=20)
     parser.opt_list('--batch_size', default=8, type=int, tunable=True, options=[4, 8, 12, 16])
     parser.opt_range('--lr_init', default=0.001, type=float, tunable=True, low=0.1, high=0.001, nb_samples=20)
 
     hyp_params = parser.parse_args()
-    hyp_params.optimize_parallel_gpu(train, gpu_ids=['0', '1'], max_nb_trials=10)
+    hyp_params.optimize_parallel_gpu(train, gpu_ids=['0', '1'], max_nb_trials=5)
