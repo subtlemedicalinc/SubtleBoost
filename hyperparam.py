6a82fb362b5925ae843675d03265fd2c9229e69f
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2019-10-30 09:47:22 -0700

    Check if specified GPUs are available

--- hyperparam.py
+++ hyperparam.py
@@ -2,7 +2,9 @@ import os
 import json
 import random
 import time
+import warnings
 from glob import glob
+import gpustat
 
 import numpy as np
 import configargparse as argparse
@@ -13,6 +15,7 @@ import subtle.subtle_io as suio
 from subtle.dnn.generators import GeneratorUNet2D
 from subtle.data_loaders import SliceLoader
 from train import train_process as train_execute
+
 
 def train_wrap(params, *args):
     ts_hash = suio.get_timestamp_hash(n=4)
@@ -26,6 +29,23 @@ def train_wrap(params, *args):
     params.job_id = ts_hash
 
     train_execute(params)
+
+def gpu_check(input_gpuids, percent_limit=0.15):
+    ip = [int(id) for id in input_gpuids]
+    stats = gpustat.GPUStatCollection.new_query().jsonify()
+
+    for gpu_info in stats['gpus']:
+        percent_util = float(gpu_info['memory.used'] / gpu_info['memory.total'])
+        if (percent_util > percent_limit) and (gpu_info['index'] in ip):
+            proc = sorted(gpu_info['processes'], key=lambda d: d['gpu_memory_usage'], reverse=True)[0]
+
+            warnings.warn('Unable to use GPU {}; currently being used by user - "{}"'.format(gpu_info['index'], proc['username']))
+
+            ip.remove(gpu_info['index'])
+    if len(ip) == 0:
+        raise Exception('None of the GPUs are available. Cannot continue hyperparameter experiment')
+    return [str(id) for id in ip]
+
 
 if __name__ == '__main__':
     seed_val = 12321
@@ -41,4 +61,6 @@ if __name__ == '__main__':
         raise ValueError('Hyperparameter search name should be specified')
 
     hparams, hyp_config = suio.get_hypsearch_params(args.hypsearch_name)
-    hparams.optimize_parallel_gpu(train_wrap, gpu_ids=hyp_config['gpus'], max_nb_trials=hyp_config['trials'])
+
+    gpu_ids = gpu_check(hyp_config['gpus'])
+    hparams.optimize_parallel_gpu(train_wrap, gpu_ids=gpu_ids, max_nb_trials=hyp_config['trials'])
