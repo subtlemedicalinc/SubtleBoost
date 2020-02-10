28be186bbf8174c5689d2efe8a0f6f9e82c3cbbf
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2020-02-10 15:34:31 -0800

    Incorporate latest changes in infer.py to easily accommodate multiple series

--- app/subtle_gad_jobs.py
+++ app/subtle_gad_jobs.py
@@ -6,7 +6,7 @@ Created on 2020/02/06
 """
 
 import os
-from typing import List, Union, Optional, get_type_hints
+from typing import Dict, Optional
 from collections import OrderedDict, namedtuple
 from itertools import groupby
 
@@ -16,8 +16,7 @@ import numpy as np
 
 from subtle.util.inference_job_utils import BaseJobType, GenericInferenceModel
 from subtle.procutil import preprocess_single_series, postprocess_single_series
-from subtle.dcmutil import pydicom_utils
-
+from subtle.dcmutil import pydicom_utils, series_utils
 
 class SubtleGADJobType(BaseJobType):
     """The job type of SubtleGAD dose reduction"""
@@ -38,7 +37,8 @@ class SubtleGADJobType(BaseJobType):
             "transform_type": "rigid",
             "normalize": True,
             "normalize_fun": "mean",
-            "mask_threshold": 0.1,
+            "noise_mask_threshold": 0.1,
+            "noise_mask_area": True,
             "scale_matching": True,
             "skip_hist_norm": True,
             "skull_strip": True,
@@ -58,16 +58,18 @@ class SubtleGADJobType(BaseJobType):
 
     SubtleGADProcConfig = namedtuple("SubtleGADProcConfig", ' '.join(list(default_processing_config_gad.keys())))
 
-    def __init__(self, name, config, model_dir, decrypt_key_hex: Optional[str] = None):
+    def __init__(self, task, model_dir, decrypt_key_hex: Optional[str] = None):
         """Initialize the job type object
 
-        :param name: see BaseJobType
-        :param config: see BaseJobType, contains the following key(s)
-                       download_base_dir: The base directory
+        :param task: subtle.dcmutils.dicom_filter.Task object of the task to execute
         :param model_dir: directory of the model to load for this job
         :param decrypt_key_hex: the decrypt key
         """
+
+        name = task.job_name
+        config = task.job_definition.exec_config
         BaseJobType.__init__(self, name, config)
+
         self._logger.info("Loading model from %s...", model_dir)
         self._model = GenericInferenceModel(model_dir=model_dir,
                                             decrypt_key_hex=decrypt_key_hex)
@@ -77,6 +79,8 @@ class SubtleGADJobType(BaseJobType):
         # update model config with app config:
         # to update tunable parameters, job parameters and app parameters
         self._model.update_config(self.config)
+
+        self.task = task
 
         # define and prepare config
         proc_config = self.default_processing_config_gad.copy()
@@ -97,12 +101,14 @@ class SubtleGADJobType(BaseJobType):
         # (keys = frame sequence name, values = pixel array)
         self._raw_input = {}
 
-    def _preprocess(self, in_dicom):
+        self._output_data = {}
+
+    def _preprocess(self, _):
         """The preprocess func
 
-        :param in_dicom: see BaseJobType
+        :param _: (in_dicom in BaseJobType by default)
         """
-        self._get_dicom_data(in_dicom)
+        self._get_dicom_data()
         #
         # get original pixel data and meta data info
         self._get_raw_pixel_data()
@@ -147,15 +153,15 @@ class SubtleGADJobType(BaseJobType):
         :param out_dicom_dir: the directory or tarball of output DICOM files
         """
         # check input size
-        for frame_seq_name in self._input_datasets:
+        for frame_seq_name in self._input_datasets[0]:
 
             # TEMP CODE
-            dict_pixel_data[frame_seq_name] = np.repeat(dict_pixel_data[frame_seq_name], len(self._input_datasets[frame_seq_name]), axis=0)
+            dict_pixel_data[frame_seq_name] = np.repeat(dict_pixel_data[frame_seq_name], len(self._input_datasets[0][frame_seq_name]), axis=0)
 
             self._logger.info('postprocess shape %s', dict_pixel_data[frame_seq_name].shape)
 
             if (
-                    len(self._input_datasets[frame_seq_name])
+                    len(self._input_datasets[0][frame_seq_name])
                     != dict_pixel_data[frame_seq_name].shape[0]
             ):
                 msg = "postprocess() got mismatched pixel data and template DICOM files"
@@ -163,120 +169,48 @@ class SubtleGADJobType(BaseJobType):
                 raise ValueError(msg)
 
         # data post processing
-        dict_pixel_data_out = self._postprocess_data(dict_pixel_data)
+        self._postprocess_data(dict_pixel_data)
         # data saving
-        self._save_data(dict_pixel_data_out, out_dicom_dir)
+        self._save_data(self._output_data, self._input_datasets[0], out_dicom_dir)
 
-    def _get_dicom_data(self, in_dicom: Union[str,
-                                              List[List[str]],
-                                              List[List[pydicom.dataset.FileDataset]]]):
+    def _get_dicom_data(self):
         """
-        read dicom data and create lists of dicom datasets
-        :param in_dicom:
+        Get dicom datasets from the DicomSeries passed through the task as a dictionary of sorted
+        datasets by frame: keys are frame sequence name and values are lists of pydicom datasets
         :return:
         """
-        in_dicom_files = []
-        list_dcm_ds = []
-        if isinstance(in_dicom, str):
-            assert os.path.isdir(
-                in_dicom
-            ), "The input dicom str is not a directory"
-            in_dicom_files = [
-                os.path.join(in_dicom, f) for f in os.listdir(in_dicom)
-            ]
-        elif isinstance(in_dicom, list):
-            assert len(in_dicom) == 1, "More than one input series"
-            if isinstance(in_dicom[0][0], str):
-                in_dicom_files = in_dicom[0]
-            elif isinstance(in_dicom[0][0], pydicom.dataset.FileDataset):
-                list_dcm_ds = in_dicom[0]
 
-        if not in_dicom_files and not list_dcm_ds:
-            raise TypeError(
-                "Wrong input type. Supported types: {}".format(
-                    get_type_hints(self._get_dicom_data)["in_dicom"]
-                )
-            )
+        assert len(self.task.dict_required_series) == 2, "More than two input series found - only zero dose and low dose series are allowed"
 
-        if not list_dcm_ds:
-            for f in in_dicom_files:
-                try:
-                    list_dcm_ds.append(pydicom.read_file(f))
-                except pydicom.errors.InvalidDicomError:
-                    # ignore non dicom files
-                    pass
+        zero_dose_series = self.task.dict_required_series['zero_dose_series']
+        low_dose_series = self.task.dict_required_series['low_dose_series']
 
-        list_dcm_ds = sorted(list_dcm_ds, key=lambda ds: ds.InstanceNumber)
-
-        def sorting_key(ds):
-            return ds.get("SequenceName", "single_frame_sequence")
-
-        list_all_dcm_ds = sorted(list_dcm_ds, key=sorting_key)
-        dict_dcm_ds_by_frame = OrderedDict(
-            [
-                (seq_name, [ds for ds in grouper])
-                for seq_name, grouper in groupby(list_all_dcm_ds, key=sorting_key)
-            ]
+        if not isinstance(zero_dose_series, series_utils.DicomSeries) or not \
+        isinstance(low_dose_series, series_utils.DicomSeries):
+            raise TypeError("Input series should be a DicomSeries object")
+        # get dictionary of sorted datasets by frame
+        self._input_datasets = (
+            zero_dose_series.get_dict_sorted_datasets(),
+            low_dose_series.get_dict_sorted_datasets()
         )
-
-        # make sure all the frames have the same slices
-        unique_slice_loc = set([ds.SliceLocation for ds in list_all_dcm_ds])
-        for frame_seq_name, list_ds_frame in dict_dcm_ds_by_frame.items():
-            assert (
-                    set([ds.SliceLocation for ds in list_ds_frame])
-                    == unique_slice_loc
-            ), "Some slices are missing for frame {}".format(frame_seq_name)
-
-        instance1_index = next(
-            (
-                index
-                for (index, ds) in enumerate(list_all_dcm_ds)
-                if str(ds.InstanceNumber) == "1"
-            ),
-            None,
-        )
-        instance2_index = next(
-            (
-                index
-                for (index, ds) in enumerate(list_all_dcm_ds)
-                if str(ds.InstanceNumber) == "2"
-            ),
-            None,
-        )
-        reverse_order = (
-            list_all_dcm_ds[instance2_index].SliceLocation
-            < list_all_dcm_ds[instance1_index].SliceLocation
-            if instance1_index is not None and instance2_index is not None
-            else False
-        )
-
-        # sort each dataset list by projected z pos
-        dict_dcm_ds_by_frame = OrderedDict(
-            [
-                (
-                    seq_name,
-                    sorted(
-                        list_ds_frame,
-                        key=pydicom_utils.get_projected_z_pos,
-                        reverse=reverse_order,
-                    ),
-                )
-                for seq_name, list_ds_frame in dict_dcm_ds_by_frame.items()
-            ]
-        )
-
-        self._input_datasets = dict_dcm_ds_by_frame
 
     def _get_raw_pixel_data(self):
-        for frame_seq_name, list_ds_frame in self._input_datasets.items():
-            list_data_fast = [
+        for frame_seq_name, _ in self._input_datasets[0].items():
+            list_data_zero = [
                 pydicom_utils.get_pixel_data_slice(dcm)[np.newaxis, :, :]
-                for dcm in list_ds_frame
+                for dcm in self._input_datasets[0][frame_seq_name]
             ]
-            self._raw_input[frame_seq_name] = np.concatenate(
-                list_data_fast, axis=0
-            )
-            #
+
+            list_data_low = [
+                pydicom_utils.get_pixel_data_slice(dcm)[np.newaxis, :, :]
+                for dcm in self._input_datasets[1][frame_seq_name]
+            ]
+
+            zero_data_np = np.concatenate(list_data_zero, axis=0)
+            low_data_np = np.concatenate(list_data_low, axis=0)
+
+            self._raw_input[frame_seq_name] = np.array([zero_data_np, low_data_np])
+
             self._logger.info(
                 "the shape of array {}".format(
                     self._raw_input[frame_seq_name].shape
@@ -296,7 +230,11 @@ class SubtleGADJobType(BaseJobType):
             input_data = data_array.copy()
 
             # write preprocess chain here
-            input_data = np.array([input_data[180:194, ...].transpose(1, 2, 0)])
+            zero_input = np.array([input_data[0, 180:187, ...].transpose(1, 2, 0)])
+            low_input = np.array([input_data[1, 180:187, ...].transpose(1, 2, 0)])
+            input_data = np.concatenate([zero_input, low_input], axis=3)
+
+            print('input data shape', input_data.shape)
 
             dict_input_data[frame_seq_name] = input_data
 
@@ -309,35 +247,32 @@ class SubtleGADJobType(BaseJobType):
         :param dict_pixel_data: dict of data to post process by frame
         :return: post processed data
         """
-        dict_pixel_data_out = {}
+
         for frame_seq_name, pixel_data in dict_pixel_data.items():
             # get 3D volume
             if len(pixel_data.shape) == 4:
                 pixel_data = pixel_data[..., 0]
 
             # write post processing here
+            self._output_data[frame_seq_name] = pixel_data
 
-            dict_pixel_data_out[frame_seq_name] = pixel_data
-
-        return dict_pixel_data_out
-
-
-    def _save_data(
-            self, dict_pixel_data, out_dicom_dir, enable_thru_plane: bool = False
-    ):
+    def _save_data(self,
+                   dict_pixel_data: Dict,
+                   dict_template_ds: Dict,
+                   out_dicom_dir: str,
+                   enable_thru_plane: bool = False):
         """
         Save pixel data to the output directory
         based on a reference dicom dataset list
         :param dict_pixel_data: dict of array of data by frame to save to output directory
+        :param dict_template_ds: dict of template dataset to use to save pixel data
         :param out_dicom_dir: path to output directory
         :return: None
         """
         # save data frame by frame but with the same series UID and a shared UID pool
         # create output directory
-        out_dicom_dir = os.path.join(
-            out_dicom_dir,
-            list(self._input_datasets.values())[0][0].SeriesInstanceUID,
-        )
+        out_dicom_dir = os.path.join(out_dicom_dir,
+                                     list(dict_template_ds.values())[0][0].StudyInstanceUID,)
         os.makedirs(out_dicom_dir, exist_ok=True)  # do not complain if exists
 
         # generate a new series UID
@@ -345,8 +280,11 @@ class SubtleGADJobType(BaseJobType):
         uid_pool = set()
         uid_pool.add(series_uid)
 
+        out_dicom_dir = os.path.join(out_dicom_dir, series_uid)
+        os.makedirs(out_dicom_dir)
+
         i_instance = 1
-        for iframe, frame_seq_name in enumerate(self._input_datasets.keys()):
+        for iframe, frame_seq_name in enumerate(dict_template_ds.keys()):
             # remove unused dimension(s)
             pixel_data = np.squeeze(dict_pixel_data[frame_seq_name])
 
@@ -377,10 +315,10 @@ class SubtleGADJobType(BaseJobType):
                     # get correct reference dataset
                     distance_to_slices = [
                         np.abs(ds.SliceLocation - slice_loc)
-                        for ds in self._input_datasets[frame_seq_name]
+                        for ds in dict_template_ds[frame_seq_name]
                     ]
                     i_ref = distance_to_slices.index(min(distance_to_slices))
-                    out_dataset = self._input_datasets[frame_seq_name][i_ref]
+                    out_dataset = dict_template_ds[frame_seq_name][i_ref]
 
                     # set slice specific metadata
                     out_dataset.SliceLocation = slice_loc
@@ -391,14 +329,14 @@ class SubtleGADJobType(BaseJobType):
                     out_dataset.SliceThickness = slice_thickness
 
                 else:
-                    out_dataset = self._input_datasets[frame_seq_name][i_slice]
+                    out_dataset = dict_template_ds[frame_seq_name][i_slice]
 
                 # add model and app info to output dicom file
                 for tag in self.private_tag:
                     out_dataset.add_new(*tag)
 
                 # set series-wide metadata
-                out_dataset.SeriesDescription = "{}{}_{}".format(
+                out_dataset.SeriesDescription = "{}{}{}".format(
                     self._proc_config.series_desc_prefix,
                     out_dataset.get("SeriesDescription", ""),
                     self._proc_config.series_desc_suffix,
