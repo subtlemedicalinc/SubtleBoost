4f1df2e0f758dec63d29250fc87dcad99c438dbf
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2020-09-30 17:14:59 -0700

    Add Edsr2D model and related code

--- train/subtle/dnn/generators/__init__.py
+++ train/subtle/dnn/generators/__init__.py
@@ -1,6 +1,7 @@
 from .mres2d import GeneratorMultiRes2D
 from .unet2d import GeneratorUNet2D
 from .unet3d import GeneratorUNet3D
+from .edsr2d import GeneratorEDSR2D
 from .edsr3d import GeneratorEDSR3D
 from .vdsr3d import GeneratorVDSR3D
 from .wdsr3d import GeneratorWDSR3D
