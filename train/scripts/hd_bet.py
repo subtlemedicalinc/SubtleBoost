3b0fbd7f5de487b6f0366351084f9bc5acc29807
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2021-03-03 13:51:56 -0800

    Change HD BET scripts to work with Tiantan data

--- train/scripts/hd_bet.py
+++ train/scripts/hd_bet.py
@@ -6,9 +6,11 @@ import tempfile
 from glob import glob
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import pydicom
 
 from HD_BET.run import run_hd_bet
-from subtle.subtle_preprocess import dcm_to_sitk
+from subtle.subtle_preprocess import dcm_to_sitk, center_crop
+import subtle.utils.io as suio
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -18,10 +20,18 @@ plt.set_cmap('gray')
 plt.rcParams['figure.figsize'] = (12, 10)
 
 def find_pre_contrast_series(dirpath_case):
-    return sorted(
-        [ser for ser in glob('{}/*BRAVO*'.format(dirpath_case))],
-        key=lambda ser: int(ser.split('/')[-1].split('_')[0])
-    )[0]
+    ser_nums = []
+    for ser_dir in glob('{}/*'.format(dirpath_case)):
+        if 'T2' in ser_dir: continue
+        dcm_file = [
+            f for f in glob('{}/**/*'.format(ser_dir), recursive=True)
+            if os.path.isfile(f) and 'XX' not in f
+        ][0]
+        dcm = pydicom.dcmread(dcm_file)
+        ser_nums.append((ser_dir, dcm.SeriesNumber))
+
+    ser_nums = sorted(ser_nums, key=lambda s: int(s[1]))
+    return ser_nums[0][0]
 
 def get_plot_image(data, mask):
     img_stack = []
@@ -40,15 +50,22 @@ def get_plot_image(data, mask):
 
     return np.hstack(img_stack)
 
+def kw_not_in(s, kws):
+    for kw in kws:
+        if kw in s:
+            return False
+    return True
+
 if __name__ == '__main__':
-    pp_base_path = '/home/srivathsa/projects/studies/gad/stanford/preprocess/data'
+    pp_base_path = '/home/srivathsa/projects/studies/gad/gad_2d/preprocess/data'
     dcm_path = pp_base_path.replace('preprocess/', '')
     save_path = os.path.join(pp_base_path, 'hdbet_masks')
     plot_path = os.path.join(save_path, 'plots')
 
     cases = sorted([
-        f.split('/')[-1].replace('.npy', '')
-        for f in glob('{}/*.npy'.format(pp_base_path))
+        f.split('/')[-1].replace('.h5', '')
+        for f in glob('{}/*.h5'.format(pp_base_path))
+        if kw_not_in(f, ['meta', 'TwoDim', 'Prisma'])
     ])
 
     processed_cases = [
@@ -62,8 +79,24 @@ if __name__ == '__main__':
         try:
             dirpath_precon = find_pre_contrast_series(os.path.join(dcm_path, case_num))
             ref_dcm = dcm_to_sitk(dirpath_precon)
-            full_data = np.load(os.path.join(pp_base_path, '{}.npy'.format(case_num)))
-            data_arr = full_data[0, :, 0]
+            ref_dims = ref_dcm.GetSize()[::-1]
+            # full_data = np.load(os.path.join(pp_base_path, '{}.npy'.format(case_num)))
+            full_data = suio.load_file(os.path.join(pp_base_path, '{}.h5'.format(case_num)))
+            data_arr = full_data[:, 0]
+
+            if data_arr.shape[0] > ref_dims[0]:
+                data_arr = center_crop(data_arr, np.zeros(ref_dims))
+            elif data_arr.shape[0] < ref_dims[0]:
+                sdiff = ref_dims[0] - data_arr.shape[0]
+                npad = []
+                if sdiff == 1:
+                    npad = [(1, 0), (0, 0), (0, 0)]
+                else:
+                    diff_part = sdiff // 2
+                    diff_rem = sdiff - diff_part
+                    npad = [(diff_part, diff_rem), (0, 0), (0, 0)]
+                data_arr = np.pad(data_arr, pad_width=npad, mode='constant', constant_values=0)
+
             data_sitk = sitk.GetImageFromArray(data_arr)
             data_sitk.CopyInformation(ref_dcm)
             mask = None
@@ -73,12 +106,12 @@ if __name__ == '__main__':
                 fpath_mask = '{}/output_mask.nii.gz'.format(tmpdir)
 
                 sitk.WriteImage(data_sitk, fpath_input)
-                run_hd_bet(fpath_input, fpath_output, mode='fast', device=4, do_tta=False)
+                run_hd_bet(fpath_input, fpath_output, mode='fast', device=0, do_tta=False)
                 mask = nib.load(fpath_mask).get_data().transpose(2, 1, 0)
 
             np.save(os.path.join(save_path, '{}.npy'.format(case_num)), mask)
             fpath_plot = os.path.join(plot_path, '{}.png'.format(case_num))
-            plt_img = get_plot_image(full_data[0], mask)
+            plt_img = get_plot_image(full_data, mask)
 
             plt.imshow(plt_img)
             plt.axis('off')
